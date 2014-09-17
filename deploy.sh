005160023a14dca2797a134dd86cf3400cdf454a
Lorenzo <fontanalorenzo@me.com>
2014-09-17 10:34:04 +0200

    rptrs rebuilding Wed Sep 17 10:34:04 CEST 2014

--- deploy.sh
+++ deploy.sh
@@ -16,5 +16,5 @@ fi
 git commit -m "$msg"
 
 # Push source and build repos.
-git push origin develop
+git push origin develop --force
 git subtree push --prefix=public git@github.com:rptrs/rptrs.github.io.git master
